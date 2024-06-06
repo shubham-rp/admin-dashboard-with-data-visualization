94a6d2a0eed79b6dd158a5069cd60bc49ba316ef
Shubham Phadte <shubhamphadte37@gmail.com>
2024-06-06 22:43:46 +0530

    layout changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <>
       <h1 className="text-2xl">Dashboard</h1>
+      <Button>Click Me</Button>
     </>
   );
 }
