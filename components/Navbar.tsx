17e34361522cdcf34089f7c3070e7525514f6c46
Shubham Phadte <shubhamphadte37@gmail.com>
2024-06-06 22:43:25 +0530

    Navbar component added

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="bg-primary dark:bg-slate-700 text-white flex py-2 px-5 justify-between">
+      Navbar
+    </div>
+  );
+}
+
+export default Navbar;
