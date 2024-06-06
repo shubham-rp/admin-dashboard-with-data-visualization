7ba3777a40e592a9c15c0290c3cfb2aca490f116
Shubham Phadte <shubhamphadte37@gmail.com>
2024-06-06 22:53:40 +0530

    Navbar changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 function Navbar() {
   return (
     <div className="bg-primary dark:bg-slate-700 text-white flex py-2 px-5 justify-between">
+      <Link href="/">
+        <Image src="" alt="Profile Logo" />
+      </Link>
       Navbar
     </div>
   );
