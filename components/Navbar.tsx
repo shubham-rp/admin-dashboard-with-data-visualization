e885536507661d89218f50a5c5fd4aa2863cb145
Shubham Phadte <shubhamphadte37@gmail.com>
2024-06-07 20:21:11 +0530

    Integration avatar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 function Navbar() {
   return (
     <div className="bg-primary dark:bg-slate-700 text-white flex py-2 px-5 justify-between">
       <Link href="/">
         <Image src="" alt="Profile Logo" />
       </Link>
+      <Avatar>
+        <AvatarImage />
+        <AvatarFallback />
+      </Avatar>
       Navbar
     </div>
   );
